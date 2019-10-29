a4dcb44efaf3bc543e6ccb32ef9ea6773420b59d
Oscar <39570765+Oscarr-Mtz@users.noreply.github.com>
2019-10-29 16:16:42 -0400

    Add files via upload

--- hashtable.cpp
+++ hashtable.cpp
@@ -4,3 +4,92 @@ CS 271
 Dr. Lall
 Project 6
 */
+
+
+#include <sstream>
+#include <string>
+#include <typeinfo>
+
+
+using namespace std;
+
+
+
+// ================================= Constructor ===============================
+template <class KeyType>
+HashTable<KeyType>::HashTable()
+//Preconditions:  N/A
+//Postcondition:  N/A
+{
+
+}
+
+
+// ============================== Copy Constructor ==============================
+template <class KeyType>
+HashTable<KeyType>::HashTable(const HashTable<KeyType>& h)
+//Preconditions:  N/A
+//Postcondition:  N/A
+{
+
+}
+
+
+// ================================= DeConstructor =============================
+template <class KeyType>
+HashTable<KeyType>::~HashTable()
+//Preconditions:  N/A
+//Postcondition:  N/A
+{
+
+}
+
+
+// ================================= Get Method ================================
+template <class KeyType>
+KeyType HashTable<KeyType>::get(const KeyType& k) const
+//Preconditions:  N/A
+//Postcondition:  N/A
+{
+
+}
+
+
+// ================================ Insert Method ==============================
+template <class KeyType>
+void HashTable<KeyType>::insert(KeyType k)
+//Preconditions:  N/A
+//Postcondition:  N/A
+{
+
+}
+
+
+// ================================ Remove Method ==============================
+template <class KeyType>
+void HashTable<KeyType>::remove(const KeyType& k)
+//Preconditions:  N/A
+//Postcondition:  N/A
+{
+
+}
+
+
+// ============================= Assignment Operator ===========================
+template <class KeyType>
+HashTable<KeyType>& HashTable<KeyType>::operator=(const HashTable<KeyType>& h)
+//Preconditions:  N/A
+//Postcondition:  N/A
+{
+
+}
+
+
+// =============================== toString Method ==============================
+template <class KeyType>
+std::string HashTable<KeyType>::toString(int slot) const
+//Preconditions:  N/A
+//Postcondition:  N/A
+{
+
+}
