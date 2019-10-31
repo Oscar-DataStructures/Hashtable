7a38b925bb7213008028c0ba68771b82578f0553
Austin Levi Burgess <burges_a1@rogue4.mathsci.denison.edu>
2019-10-31 02:22:39 -0400

    sdf

--- scrabble.py
+++ scrabble.py
@@ -1,9 +1,25 @@
 """
-Oscar Martinez
+Austin Burgess, Oscar Martinez
 CS 271
 Dr. Lall
 Project 6
 """
 
+def readFile(fileName):
+    open(fileName, "r")
+    print(file.read())
+import math
+
+def hash(k):
+    knum = 0         # ensure we have a number here
+    for letter in k:
+      knum = knum + ord(letter) - 96
+    m = 8191                    # big prime number
+    A = (math.sqrt(5) - 1) / 2  # Knuth
+
+    return (m * (knum * A // 1)) // 1000
+
 def main():
-  return 0
+    readFile("scrabble.txt")
+
+main()
